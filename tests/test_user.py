cecaa9e1456c00018484feb6b5d23b90814769db
Иван <ivanshiryaev@MacBook-Air-Ivan.local>
2025-12-02 20:06:15 +0300

    Тесты

---
+++ tests/test_user.py
@@ -0,0 +1,11 @@
+from src.taskClass import User, Book
+
+def test_user_borrow_and_return():
+    user = User("Анна")
+    book = Book("Title", "Author", 2000)
+    assert user.borrow(book) is True
+    assert book.get_available() is False
+    assert len(user.get_borrowed_books()) == 1
+    assert user.return_book(book) is True
+    assert book.get_available() is True
+    assert len(user.get_borrowed_books()) == 0
