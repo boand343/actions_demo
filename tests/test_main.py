69454df16f24de06fc10fd53942d06d62cf6a88c
Иван <ivanshiryaev@MacBook-Air-Ivan.local>
2025-12-02 21:22:59 +0300

    Ещё изменения

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,6 @@
 import sys
 import os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__),
-                                '..'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 import unittest
 
 from src.main import add, subtract
