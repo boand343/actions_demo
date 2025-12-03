6266575cca2b4db064a6f7f2a2cff3c0f0ede80a
danilkalog <pisma4dan4ik@gmail.com>
2025-12-03 18:03:51 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,34 +1,37 @@
-import pytest
-from src.main import add, subtract
+import sys
+import os
+
+# Добавляем папку src в путь поиска модулей
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
+
+from main import add, subtract
 
 
-class TestMathFunctions:
-    """Тесты для математических функций"""
-    
-    def test_add_positive_numbers(self):
-        """Тест сложения положительных чисел"""
-        assert add(2, 3) == 5
-        assert add(0, 0) == 0
-    
-    def test_add_negative_numbers(self):
-        """Тест сложения отрицательных чисел"""
-        assert add(-2, -3) == -5
-        assert add(-2, 3) == 1
-    
-    def test_subtract_positive_numbers(self):
-        """Тест вычитания положительных чисел"""
-        assert subtract(5, 3) == 2
-        assert subtract(3, 5) == -2
-    
-    def test_subtract_negative_numbers(self):
-        """Тест вычитания отрицательных чисел"""
-        assert subtract(-5, -3) == -2
-        assert subtract(-5, 3) == -8
-    
-    def test_add_large_numbers(self):
-        """Тест сложения больших чисел"""
-        assert add(1000000, 2000000) == 3000000
-    
-    def test_subtract_large_numbers(self):
-        """Тест вычитания больших чисел"""
-        assert subtract(5000000, 3000000) == 2000000
+def test_add_positive():
+    """Тест сложения положительных чисел"""
+    assert add(2, 3) == 5
+
+
+def test_add_negative():
+    """Тест сложения отрицательных чисел"""
+    assert add(-2, -3) == -5
+
+
+def test_add_mixed():
+    """Тест сложения положительного и отрицательного"""
+    assert add(5, -3) == 2
+
+
+def test_subtract_positive():
+    """Тест вычитания положительных чисел"""
+    assert subtract(5, 3) == 2
+
+
+def test_subtract_negative():
+    """Тест вычитания отрицательных чисел"""
+    assert subtract(-5, -3) == -2
+
+
+def test_subtract_mixed():
+    """Тест вычитания положительного и отрицательного"""
+    assert subtract(5, -3) == 8
